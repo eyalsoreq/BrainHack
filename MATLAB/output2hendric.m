5e0cefec96cdeb2e6a909eb744314d0619c8c2d3
Eyal Soreq <eyalsoreq@gmail.com>
2017-03-19 11:07:46 +0000

    new

--- MATLAB/output2hendric.m
+++ MATLAB/output2hendric.m
@@ -1,4 +1,4 @@
-function [y1,y2] = output2hendric(X,prefix,outdir,win,n)
+function [y1,y2] = output2hendric(X,Y,prefix,outdir,win,n)
 fmt = repmat({'%4.3f '},1,n);
     fmt = [fmt{:}];fmt(end:end+2)=';\n';
     fid = fopen(sprintf('%s%s%s_frequencies.txt',outdir,filesep,prefix),'w');
@@ -12,19 +12,20 @@ if ~isempty(win)
     y1 =  zeros(win+1,n);
     y2 =  zeros(win+1,n);
 else 
-    tmp = X;
+    tmp1 = X;
+    tmp2 = Y;
     y1 =  zeros(size(X));
     y2 =  zeros(size(X));
 end
 for ii=1:n
-y1(:,ii) = c3nl_scale(tmp(:,ii),'col',(ii-1)*100,ii*100) ;
+    y1(:,ii) = c3nl_scale(tmp1(:,ii),'col',(ii-1)*100,ii*100) ;
 end
 %figure();plot(y);hold on
 fprintf(fid,fmt,y1');
 fclose(fid);
 fid = fopen(sprintf('%s%s%s_amplitudes.txt',outdir,filesep,prefix),'w');
 for ii=1:n
-y2(:,ii) = c3nl_scale(tmp(:,ii),'col',1/(ii+1),1/(ii)) ;
+y2(:,ii) = c3nl_scale(tmp2(:,ii),'col',1/(ii+1),1/(ii)) ;
 end
 fprintf(fid,fmt,y2');
 fclose(fid);
